638afdb0fc66abf630bfb50d98b0061731b1f751
Danny Yang <dannyyaaj@gmail.com>
2022-04-21 22:28:45 -0500

    practice using get, contains, should exist methods

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,16 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  })
+
+  it('should have a form', () => {
+    cy.get('[data-test="new-item-input"]').should('exist');
+  });
+
+  it('should have the words "Add Item"', () => {
+    cy.contains('Add Item');
+  })
+
+});
