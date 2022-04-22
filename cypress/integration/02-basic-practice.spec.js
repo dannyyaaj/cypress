fa4a07cde9b40d318e78a9af345b16d7a2260898
Danny Yang <dannyyaaj@gmail.com>
2022-04-22 00:18:50 -0500

    work on basic practice exercise number 2

--- cypress/integration/02-basic-practice.spec.js
+++ cypress/integration/02-basic-practice.spec.js
@@ -6,26 +6,70 @@ describe('Basic Practice', () => {
   });
 
   describe('Adding a new item', () => {
-    it('should put a new item on the page after clicking on "Add Item"', () => {});
+    it('should put a new item on the page after clicking on "Add Item"', () => {
+      const item = 'Good Attitude';
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('[data-test="add-item"]').click();
+      cy.contains(item);
 
-    it('should put a new item in the "Unpacked Items" list', () => {});
+    });
 
-    it('should put a new item as the last item in the "Unpacked Items" list', () => {});
+    it('should put a new item in the "Unpacked Items" list', () => {
+      const item = 'Good Attitude';
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit();
+
+      cy.get('[data-test="items-unpacked"]').contains(item);
+    });
+
+    it('should put a new item as the last item in the "Unpacked Items" list', () => {
+      const item = 'Good Attitude';
+      cy.get('[data-test="new-item-input"]').type(item);
+      cy.get('form').submit();
+      
+      cy.get('[data-test="items-unpacked"] li').last().contains(item);
+    });
   });
 
   describe('Filtering items', () => {
-    it('should show items that match whatever is in the filter field', () => {});
+    it('should show items that match whatever is in the filter field', () => {
+      cy.get('[data-test="filter-items"]').type('tooth');
+      cy.contains('Tooth Brush');
+      cy.contains('Tooth Paste');
+    });
 
-    it('should hide items that do not match whatever is in the filter field', () => {});
+    it('should hide items that do not match whatever is in the filter field', () => {
+      cy.get('[data-test="filter-items"]').type('tooth');
+      cy.contains('iPhone Charger').should('not.exist');
+    });
   });
 
   describe('Removing items', () => {
     describe('Remove all', () => {
-      it('should remove all of the items', () => {});
+      it('should remove all of the items', () => {
+        cy.get('[data-test="remove-all"]').click();
+
+        cy.get('[data-test="items"] li').should('not.exist');
+      });
+
+      it('should remove all of the items (alternate)', () => {
+        cy.get('[data-test="remove-all"]').click();
+
+        cy.get('[data-test="items-unpacked"]').contains('No items to show.');
+        cy.get('[data-test="items-packed"]').contains('No items to show.');
+      });
     });
 
     describe('Remove individual items', () => {
-      it('should have a remove button on an item', () => {});
+      it('should have a remove button on an item', () => {
+        cy.get('[data-test="items"] li').find('[data-test="remove"]')
+      });
+
+      it('should have a remove button on each item (literally)', () => {
+        cy.get('[data-test="items"] li').each((li) => {
+          cy.wrap(li).find('[data-test="remove"]').should('exist');
+        })
+      });
 
       it('should remove an item from the page', () => {});
     });
